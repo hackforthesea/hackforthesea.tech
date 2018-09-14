ff18804e82cca3e1524b1ce85c0abc6369d7af7a
Mark Henderson <henderson.mark@gmail.com>
2018-09-14 11:01:43 -0400

    More removals

--- client/context_processors.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-
-def site(request):
-    return {'SITE_URL': settings.SITE_URL}
-
-def jupyterhub_port(request):
-    return {'JUPYTERHUB_PORT': settings.JUPYTERHUB_PORT}
